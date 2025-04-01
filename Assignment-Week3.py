7f2894bfd8b3feb3bd1705182e76672247541f4c
7thSage-goose <thandolwethundhlovu8@gmail.com>
2025-04-01 13:16:47 +0200

    added Assignment file

---
+++ Assignment-Week3.py
@@ -0,0 +1,6 @@
+price = int(input("Enter the original price: "))
+discount_percent = int(input("Enter the discount percentage: "))
+def discount_calculation(price,discount_percent):
+    if discount_percent> 20.0 :
+        
+        
