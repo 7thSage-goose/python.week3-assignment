72a4aff9bbc569a6e0aa6cea49a49823a23835e1
7thSage-goose <thandolwethundhlovu8@gmail.com>
2025-04-01 17:44:23 +0200

    completed Assignment

--- Assignment-Week3.py
+++ Assignment-Week3.py
@@ -1,6 +1,10 @@
-price = int(input("Enter the original price: "))
-discount_percent = int(input("Enter the discount percentage: "))
-def discount_calculation(price,discount_percent):
-    if discount_percent> 20.0 :
-        
-        
+def calculate_discount(price,discount_percent):
+    #the function checks if the percentage is greater or equal to 20%
+    if discount_percent >= 20:
+     discount_amount= price * (discount_percent/100)
+     result = price-discount_amount 
+     return f"The priceis now {result}"
+    else:
+        return f"The price is {price}" 
+
+print(calculate_discount(price= float(input("Enter the original price: ")),discount_percent= float(input("Enter a percentage: "))))
